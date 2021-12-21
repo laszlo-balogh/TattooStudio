3fba02e6fbbafa16536eeffeb477aa473620f0a0
laszlo-balogh <balaszlo444@gmail.com>
2021-12-22 00:04:24 +0100

    Add project files.

---
+++ TattooStudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TattooStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
