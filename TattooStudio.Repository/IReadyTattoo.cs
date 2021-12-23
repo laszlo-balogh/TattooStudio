8e9756750d552a5a82c68f4bfe84b02af24cffd2
laszlo-balogh <balaszlo444@gmail.com>
2021-12-23 20:51:09 +0100

    new calss's repo

---
+++ TattooStudio.Repository/IReadyTattoo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TattooStudio.Models;
+
+namespace TattooStudio.Repository
+{
+    interface IReadyTattoo
+    {
+        void Create(ReadyTattoo readyTattoo);
+        void Delete(int id);
+        ReadyTattoo Read(int id);
+        IQueryable<ReadyTattoo> ReadAll();
+    }
+}
